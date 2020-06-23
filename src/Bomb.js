71cf6a45916751df3a58ed5c667f01185fe647ef
sap2411 <steven.parsons.us@gmail.com>
2020-06-23 09:10:02 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    
+    // our increment method makes use of the 'setState' method, which is what we use to alter state
+    increment = () => {
+        const newCount = this.state.count + 1
+        this.setState({
+            count: newCount
+        })
+    }
+    
+    render() {
+        return (
+            this.state.secondsLeft > 0 ? (<div>{this.state.secondsLeft} seconds left before I go boom!</div>) : (<div>Boom!</div>)
+        )
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,27 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    
+    // our increment method makes use of the 'setState' method, which is what we use to alter state
+    // increment = () => {
+    //     const newCount = this.state.count + 1
+    //     this.setState({
+    //         count: newCount
+    //     })
+    // }
+    
+    
+    
+    render() {
+        return (
+            <div>I am on slide {this.state.currentSlideIndex}</div>
+        )
+    }
+}
